9db03677bc37f96e59d43ae32b36f90da0eec0c7
TMT2010 <t_t2010@yahoo.com>
2022-04-12 23:49:07 -0400

    added folder structure
    added folders/files of the weather app from desktop and VS Code

---
+++ src/script.js
@@ -0,0 +1,79 @@
+function formatDate(date) {
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let min = date.getMinutes();
+  if (min < 10) {
+    min = `0${min}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let day = days[dayIndex];
+
+  return `${day} ${hour}:${min}`;
+}
+
+let dateElement = document.querySelector("#todaysDate");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+function getWeather(response) {
+  document.querySelector("#mainCity").innerHTML = response.data.name;
+  document.querySelector(".main-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weatherDescription").innerHTML =
+    response.data.weather[0].main;
+
+  document.querySelector("#pressure").innerHTML = Math.round(
+    response.data.main.pressure
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+}
+
+function search(cityName) {
+  let apiKey = "c0439477745b320addbedcd6c06eaeea";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(getWeather);
+}
+
+function submitCity(event) {
+  event.preventDefault();
+  let cityName = document.querySelector("#searchBox").value;
+  search(cityName);
+}
+
+search("Charlotte");
+
+let searchBox = document.querySelector("#searchForm");
+searchBox.addEventListener("submit", submitCity);
+
+function displayTemp(response) {
+  console.log();
+  let temp = Math.round(response.data.main.temp);
+
+  let currentLocation = document.querySelector("#locationButton");
+  currentLocation.innerHTML = temp;
+}
+
+function showLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "imperial";
+  let apiKey = "c9a1d00cde81ec7b52d8f92a4b7a5348";
+  let endPoint = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiUrl = `${endPoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayTemp);
+}
+
+navigator.geolocation.getCurrentPosition(showLocation);
